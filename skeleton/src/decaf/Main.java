f7e54c63f33ad22f051ba58dc99d5e90a3c9b53d
RicardoHortencio <ricardohortencio@outlook.com>
2017-06-14 11:59:02 -0300

    Parse Tree

--- skeleton/src/decaf/Main.java
+++ skeleton/src/decaf/Main.java
@@ -2,10 +2,23 @@ package decaf;
 
 import java.io.*;
 //import antlr.Token;
+
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+//import org.antlr.v4.runtime.tree.ParseTreeWalker;
+// import org.atlr.v4.runtime.tree.gui.TreeViewer;
+import org.antlr.v4.gui.TreeViewer;
+
 import java6035.tools.CLI.*;
+
+
+
 
 class Main {
     public static void main(String[] args) {
@@ -107,20 +120,49 @@ class Main {
         	            System.out.println(CLI.infile+" "+e);
         	            lexer.skip();
         	        } 
-
 				
 
 
         		}
 
         	}
-        	else if (CLI.target == CLI.PARSE || CLI.target == CLI.DEFAULT)
+        	  else if (CLI.target == CLI.PARSE || CLI.target == CLI.DEFAULT)
         	{
         		DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
 				CommonTokenStream tokens = new CommonTokenStream(lexer);
         		DecafParser parser = new DecafParser(tokens);
                 parser.program();
-        	}
+        	} else if (CLI.target == CLI.INTER) { 
+	          DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
+		  CommonTokenStream tokens = new CommonTokenStream(lexer);
+		  DecafParser parser = new DecafParser(tokens);
+
+		// Create Tree View
+		// Source: https://stackoverflow.com/questions/23809005/how-to-display
+		
+		ParseTree tree = parser.program();
+
+		//show AST in console
+
+		System.out.println(tree.toStringTree(parser));
+		
+		if(CLI.debug) {
+		//show AST in GUI
+		JFrame frame = new JFrame("Antlr AST");
+		JPanel panel = new JPanel();
+		TreeViewer viewr = new TreeViewer(Arrays.asList(
+		parser.getRuleNames()),tree);
+		viewr.setScale(1.5); //scale a litler
+		panel.add(viewr);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(200,200);
+		frame.setVisible(true);
+
+	}
+	}
+
+
         	
         } catch(Exception e) {
         	// print the error:
